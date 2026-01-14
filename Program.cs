53625339dbaff7ece11606d4c0171a13fc770008
burgasa <burgasa32@gmail.com>
2026-01-14 12:15:13 +0200

    Antoio new program

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrinterQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> printerQueue = new Queue<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "print")
+                {
+                    // Испечати ги сите преостанати фајлови во редот
+                    while (printerQueue.Count > 0)
+                    {
+                        Console.WriteLine(printerQueue.Dequeue());
+                    }
+                    break;
+                }
+                else if (input == "cancel")
+                {
+                    if (printerQueue.Count > 0)
+                    {
+                        string canceledFile = printerQueue.Dequeue();
+                        Console.WriteLine($"Canceled {canceledFile}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Standby");
+                    }
+                }
+                else
+                {
+                    // Нов фајл - стави го во ред
+                    printerQueue.Enqueue(input);
+                }
+            }
+        }
+    }
+}
